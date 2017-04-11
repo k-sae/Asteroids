2679c3548450af310c699d279f1dd118a86ea32a
Belal <billy.fcih@yahoo.com>
2017-04-11 23:24:21 +0200

    Render the space-ship

--- Main.hs
+++ Main.hs
@@ -8,7 +8,7 @@ height = 700
 offset = 100
 
 window :: Display
-window = InWindow "Pong" (width, height) (offset, offset)
+window = InWindow "Asteroids" (width, height) (offset, offset)
 
 background :: Color
 background = white
@@ -21,7 +21,7 @@ main = play window background fps initialState render handleKeys update
 
 -------------------- the custom datatypes --  TODO :: decide if we should use deriving Show 
 data Player = Player 
-    { degree :: Float   --   the degree will be W.R.T X-axis like this   (>)  <- space ship at degree 0
+    { degree :: Float            --  the degree will be W.R.T X-axis like this   (>)  <- space ship at degree 0
     , plSpeed  :: (Float, Float)  -- speed W.R.T (x and y axes)
     , plLocation :: (Float, Float) -- location W.R.T (x and y axes)
     }
@@ -59,13 +59,13 @@ initialState = Game
 
 initializePlayer :: Player
 initializePlayer = Player
-    { degree = 0
+    { degree = 250
     , plSpeed = (0,0)
     , plLocation = (0,0)
     }
 
 -- the game foreach loop
-update ::  Float -> AsteroidsGame -> AsteroidsGame 
+update :: Float -> AsteroidsGame -> AsteroidsGame 
 update seconds as = as
 
 -- handle game events like thrust button etc
@@ -74,4 +74,15 @@ handleKeys _ game = game
 
 render :: AsteroidsGame  -- ^ The game state to render.
        -> Picture   -- ^ A picture of this game state.
-render game = pictures []
+render game = pictures
+ [
+   mkShip black white (plLocation (player game)) $ (degree (player game))
+ ]
+ where
+  mkShip :: Color -> Color -> (Float, Float) -> Float -> Picture
+  mkShip col col2 (x,y) degree = pictures
+   [
+     translate x y $ color col $ arcSolid degree (degree+40) 40,
+     translate x y $ color col2 $ arcSolid (degree+10) (degree+30) 35
+   ]
+
