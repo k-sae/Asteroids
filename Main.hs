d172c4fdf54383935429eecb333d90a443d04a13
Belal <billy.fcih@yahoo.com>
2017-04-13 21:23:37 +0200

    Finish ship rotation

--- Main.hs
+++ Main.hs
@@ -52,13 +52,25 @@ update seconds game | (gameMode game) == Menu = updateMenue seconds game
 -- handle game events like thrust button etc
 handleKeys :: Event -> AsteroidsGame -> AsteroidsGame
 handleKeys (EventKey (Char '1') _ _ _) game                            -- Enter singleplayer mode when press '1'
- | (gameMode game) == Menu = game {gameMode = Single}                  -- Only if he is in Meny mode
+ | (gameMode game) == Menu = game {gameMode = Single}                  -- Only if he is in Menu mode
 handleKeys (EventKey (Char '2') _ _ _) game                            -- Enter cooperative mode when press '2'
- | (gameMode game) == Menu = game {gameMode = Cooperative}             -- Only if he is in Meny mode
+ | (gameMode game) == Menu = game {gameMode = Cooperative}             -- Only if he is in Menu mode
 handleKeys (EventKey (Char '3') _ _ _) game                            -- Enter versus mode when press '3'
- | (gameMode game) == Menu = game {gameMode = Versus}                  -- Only if he is in Meny mode
+ | (gameMode game) == Menu = game {gameMode = Versus}                  -- Only if he is in Menu mode
 handleKeys (EventKey (Char 'q') _ _ _) game = game {gameMode = Menu}   -- Return to the menu and quit the game when press 'q'
+
+handleKeys (EventKey (Char 'd') _ _ _) game                                -- Rotate the ship Clock-Wise when press 'd'
+ | (gameMode game) /= Menu = game { player = rotatePl (-10) (player game)} -- Only if he is not in Menu mode
+handleKeys (EventKey (Char 'a') _ _ _) game                                -- Rotate the ship Anti_Clock-Wise when press 'a'
+ | (gameMode game) /= Menu = game { player = rotatePl (10) (player game)}  -- Only if he is not in Menu mode
+
 handleKeys _ game = game
+
+--The x value will be the rotatingBy value!
+rotatePl :: Float -> Player -> Player
+rotatePl x player = player {degree = newdegree}
+ where 
+  newdegree = ((degree player) + x )
 
 render :: AsteroidsGame  --- update the render like the update function in order to behave like the update function
        -> Picture   
@@ -72,15 +84,14 @@ render game
 
  | otherwise = pictures
    [
-     mkShip black white (plLocation (player game)) $ (degree (player game))
+     mkShip black black (plLocation (player game)) $ (degree (player game))
    ]
    where
     mkShip :: Color -> Color -> (Float, Float) -> Float -> Picture
     mkShip col col2 (x,y) degree = pictures
      [
-       translate x y $ color red $ arcSolid (degree+15) (degree+25) 45,
-       translate x y $ color col $ arcSolid degree (degree+40) 40,
-       translate x y $ color col2 $ arcSolid (degree+10) (degree+30) 35
+       translate x y $ color col $ sectorWire degree (degree+40) 40,
+       translate x y $ color col2 $ sectorWire (degree+10) (degree+30) 35
 
      ]
 
