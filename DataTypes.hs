aeeea8787a2da7bdea936b9e25cd37ac5b2d3764
kareem2048 <karim.salah2048@gmail.com>
2017-04-13 11:15:58 +0200

    add datatypes file

---
+++ DataTypes.hs
@@ -0,0 +1,39 @@
+module DataTypes where
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+
+-------------------- the custom datatypes --  TODO :: decide if we should use deriving Show 
+data Player = Player 
+    { degree :: Float            --  the degree will be W.R.T X-axis like this   (>)  <- space ship at degree 0
+    , plSpeed  :: (Float, Float)  -- speed W.R.T (x and y axes)
+    , plLocation :: (Float, Float) -- location W.R.T (x and y axes)
+    , rotatingBy :: Int
+    , firingSpeed :: Int               -- fire speed for more fun :D
+    , isrotating :: Bool
+    , isFiring :: Bool
+    , projectiles :: [Projectile]
+    , firemode :: Int                  -- fire mode may be removed later 
+    }
+
+
+data Projectile = Projectile
+     { prSpeed  :: (Float, Float)  -- speed W.R.T (x and y axes)
+     , prLocation :: (Float, Float) -- location W.R.T (x and y axes)
+     , prLifeTime :: Int
+     }
+
+
+data Asteroid = Asteroid
+     { size :: Int
+     , aLocation :: (Float, Float) -- location W.R.T (x and y axes)
+     , aSpeed  :: (Float, Float)  -- speed W.R.T (x and y axes)
+     , radius :: Float        -- decide on whatever the team see right (circle or quadrilateral)
+     }
+
+data AsteroidsGame = Game 
+     { player :: Player
+     , gameMode :: GameMode
+     , asteroids   :: [Asteroid]
+     }
+
+data GameMode = Menu | Single | Cooperative | Versus
