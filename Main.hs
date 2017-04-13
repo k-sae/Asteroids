dc5d7a8d98331c25c8cdf0e8fa722e515c7e8458
Belal <billy.fcih@yahoo.com>
2017-04-13 19:53:33 +0200

    Add an initial menu

--- Main.hs
+++ Main.hs
@@ -51,21 +51,36 @@ update seconds game | (gameMode game) == Menu = updateMenue seconds game
 
 -- handle game events like thrust button etc
 handleKeys :: Event -> AsteroidsGame -> AsteroidsGame
+handleKeys (EventKey (Char '1') _ _ _) game                            -- Enter singleplayer mode when press '1'
+ | (gameMode game) == Menu = game {gameMode = Single}                  -- Only if he is in Meny mode
+handleKeys (EventKey (Char '2') _ _ _) game                            -- Enter cooperative mode when press '2'
+ | (gameMode game) == Menu = game {gameMode = Cooperative}             -- Only if he is in Meny mode
+handleKeys (EventKey (Char '3') _ _ _) game                            -- Enter versus mode when press '3'
+ | (gameMode game) == Menu = game {gameMode = Versus}                  -- Only if he is in Meny mode
+handleKeys (EventKey (Char 'q') _ _ _) game = game {gameMode = Menu}   -- Return to the menu and quit the game when press 'q'
 handleKeys _ game = game
 
-render :: AsteroidsGame  --- update the render like the update function in order to behave like the the update function
+render :: AsteroidsGame  --- update the render like the update function in order to behave like the update function
        -> Picture   
-render game = pictures
- [
-   mkShip black white (plLocation (player game)) $ (degree (player game))
- ]
- where
-  mkShip :: Color -> Color -> (Float, Float) -> Float -> Picture
-  mkShip col col2 (x,y) degree = pictures
+render game 
+ | (gameMode game) == Menu = pictures
    [
-     translate x y $ color red $ arcSolid (degree+15) (degree+25) 45,
-     translate x y $ color col $ arcSolid degree (degree+40) 40,
-     translate x y $ color col2 $ arcSolid (degree+10) (degree+30) 35
-
+     translate (-450) 200 (text "(1)SinglePlayer"),
+     translate (-450) 0 (text "(2)Cooperative"),
+     translate (-450) (-200) (text "(2)Versus")
    ]
+
+ | otherwise = pictures
+   [
+     mkShip black white (plLocation (player game)) $ (degree (player game))
+   ]
+   where
+    mkShip :: Color -> Color -> (Float, Float) -> Float -> Picture
+    mkShip col col2 (x,y) degree = pictures
+     [
+       translate x y $ color red $ arcSolid (degree+15) (degree+25) 45,
+       translate x y $ color col $ arcSolid degree (degree+40) 40,
+       translate x y $ color col2 $ arcSolid (degree+10) (degree+30) 35
+
+     ]
 
